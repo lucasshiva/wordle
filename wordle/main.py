72eb17c08387e5bb21dbef98c4ed6059b04d2173
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2022-04-26 20:55:56 -0300

    cli: setup the cli.

--- wordle/main.py
+++ wordle/main.py
@@ -1,5 +1,21 @@
+from argparse import ArgumentParser
+
+from wordle.modes import Mode
+
+
 def main() -> None:
-    pass
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-m", "--mode", action="store", type=str, choices=["solo", "duo", "quad"], default="solo"
+    )
+
+    args = parser.parse_args()
+    mode = Mode.from_name(args.mode)
+    start_wordle(mode)
+
+
+def start_wordle(mode: Mode) -> None:
+    print(f"You are playing Wordle on {mode.name} mode.")
 
 
 if __name__ == "__main__":
---
+++ wordle/modes.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+default_settings = {
+    "solo": {"max_guesses": 6, "puzzles_amount": 1},
+    "duo": {"max_guesses": 7, "puzzles_amount": 2},
+    "quad": {"max_guesses": 9, "puzzles_amount": 4},
+}
+
+
+@dataclass
+class Mode:
+    name: str
+    max_guesses: int
+    puzzles_amount: int
+
+    @classmethod
+    def from_name(cls, name: str) -> "Mode":
+        """Return an instance of `Mode` from a string."""
+        if name not in default_settings:
+            raise ValueError(f"Mode {name} not found.")
+
+        mode_settings = default_settings[name]
+        return cls(name=name, **mode_settings)
